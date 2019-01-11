d02edc1a5080f00f6fba4a5ea150f48c6db3f43e
Nuno Maduro <enunomaduro@gmail.com>
2019-01-11 17:01:11 +0100

    wip

--- src/Application/Http/Middlewares/ConvertEmptyStringsToNull.php
+++ src/Application/Http/Middlewares/ConvertEmptyStringsToNull.php
@@ -5,11 +5,7 @@ namespace Application\Http\Middlewares;
 
 use Narration\Http\Contracts\BeforeRequestHandlingInterface;
 use Narration\Http\Contracts\Middleware;
-use Psr\Http\Message\MessageInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 
 class ConvertEmptyStringsToNull implements Middleware, BeforeRequestHandlingInterface
 {
@@ -18,8 +14,6 @@ class ConvertEmptyStringsToNull implements Middleware, BeforeRequestHandlingInte
         foreach ($request->getAttributes() as $key => $value) {
             $request = $request->withAttribute($key, ($value === '') ? null : $value);
         }
-
-        $request = $request->withAttribute('fpp', 'bar');
 
         return $next($request);
     }
--- src/Application/Http/RequestHandlers/Get.php
+++ src/Application/Http/RequestHandlers/Get.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Application\Http\RequestHandlers;
 
 use Application\Http\Middlewares\ConvertEmptyStringsToNull;
-use Application\Http\Middlewares\TestAfter;
-use Application\Http\Middlewares\TestAfterSecond;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -16,7 +14,6 @@ final class Get implements RequestHandlerInterface
     public $middleware = [
         ConvertEmptyStringsToNull::class,
     ];
-
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
