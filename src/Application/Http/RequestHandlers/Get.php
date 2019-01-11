bb1fcf73aaab207b613d5068361e6ec5e2273944
Devin Beeuwkes <devinbeeuwkes7@gmail.com>
2019-01-11 13:57:26 +0100

    Add middleware pipeline

--- src/Application/Http/RequestHandlers/Get.php
+++ src/Application/Http/RequestHandlers/Get.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Application\Http\RequestHandlers;
 
+use Application\Http\Middlewares\ConvertEmptyStringsToNull;
+use Application\Http\Middlewares\TestAfter;
+use Application\Http\Middlewares\TestAfterSecond;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 final class Get implements RequestHandlerInterface
 {
+    public $middleware = [
+        ConvertEmptyStringsToNull::class,
+    ];
+
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $response = new \Zend\Diactoros\Response\JsonResponse(['nuno' => 'nuno']);
