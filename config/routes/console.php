3bfc612c9b9d25c99583b0c5204176332e2a4e5b
Nuno Maduro <enunomaduro@gmail.com>
2019-01-24 22:33:12 +0100

    wip

---
+++ config/routes/console.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+/**
+ * The Console component allows you to create command-line commands.
+ * Your console commands can be used for any recurring task, such
+ * as cronjobs, imports, or other batch jobs.
+ */
+return [
+    // ..
+];
