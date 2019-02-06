36648886bc0268cfcf3947a4d2e9f0b04e17e1f8
Nuno Maduro <enunomaduro@gmail.com>
2019-02-06 17:17:19 +0100

    wip

--- src/Application/Http/RequestHandlers/Index.php
+++ src/Application/Http/RequestHandlers/Index.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Application\Http\RequestHandlers;
 
-use Domain\Contracts\Repositories\TaskRepositoryInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 
 final class Index
 {
