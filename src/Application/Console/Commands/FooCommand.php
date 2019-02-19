391527a80800e590554aeab6eab5216a1b91fbde
Nuno Maduro <enunomaduro@gmail.com>
2019-02-19 21:41:33 +0100

    wip

--- src/Application/Console/Commands/FooCommand.php
+++ src/Application/Console/Commands/FooCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Application\Console\Commands;
 
-use Domain\Contracts\Repositories\TaskRepositoryInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
