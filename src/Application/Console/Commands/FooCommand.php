4d34efde3deb5253f766871c51ad0808a2a7b07e
Nuno Maduro <enunomaduro@gmail.com>
2019-02-19 21:39:00 +0100

    wip

--- src/Application/Console/Commands/FooCommand.php
+++ src/Application/Console/Commands/FooCommand.php
@@ -11,6 +11,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class FooCommand
 {
     /**
+     * Handle the given input.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface $output
+     *
      * @return void
      */
     public function __invoke(InputInterface $input, OutputInterface $output): void
