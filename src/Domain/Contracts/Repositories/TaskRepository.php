5316be6b25793d9b4079daf66b539b3e4676c202
Nuno Maduro <enunomaduro@gmail.com>
2019-03-10 10:20:21 +0100

    wip

--- src/Domain/Contracts/Repositories/TaskRepository.php
+++ src/Domain/Contracts/Repositories/TaskRepository.php
@@ -6,6 +6,6 @@ namespace Domain\Contracts\Repositories;
 
 use Domain\Entities\Task;
 
-interface TaskRepositoryInterface
+interface TaskRepository
 {
 }
---
+++ src/Infrastructure/Persistence/Repositories/DatabaseTaskRepository.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Infrastructure\Persistence\Repositories;
+
+use Domain\Contracts\Repositories\TaskRepository;
+
+final class DatabaseTaskRepository implements TaskRepository
+{
+}
